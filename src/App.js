17c162c09dce4ce0b161b29969a313519f78ca19
rinush <rinu098@gmail.com>
2021-04-07 12:03:57 -0400

    "Created basic profile"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Login from './components/login'
-import Register from './components/register'
 import Profile from './components/profile'
+import profile from './imgs/profile.jpg'
+import event1 from './imgs/event1.jpg'
+import event2 from './imgs/event2.jpg'
+import event3 from './imgs/event3.jpg'
+import media1 from './imgs/media1.jpg'
+import media2 from './imgs/media2.jpg'
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Profile imgPath = {profile} events = {[event1, event2, event3]} media = {[media1, media2]} />
     </div>
   );
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
+import './profile-style.css'
+import BasicInfo from './basic-info.js'
+import Media from './media.js'
+import Event from './event.js'
 
-//Profile
-
-const Profile = () => {
+const Profile = ({imgPath, events, media}) => {
 
 return (
 
@@ -22,6 +24,61 @@ return (
 </div>
 <div class="col-sm-2">
 <Route path="/">Login/SignUp</Route>
+</div>
+
+</div>
+
+<div class="row mt-3">
+
+<div class="col-sm-4 profile">
+
+<div>
+<img src={imgPath} class="profile-pic" />
+</div>
+
+<div class="mt-3">
+<button type="button" class="btn btn-primary">Edit Profile</button>
+</div>
+
+</div>
+
+<div class="col-sm-8">
+
+<div class="row">
+
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Basic Info</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Media</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">My Events</a>
+  </li>
+</ul>
+
+</div>
+
+<div class="row mt-3">
+
+<BasicInfo fname={"Shaili"}
+           lname={"Gandhi"}
+           email={"abd@gmail.com"}
+           password={"123456"}
+           dob={"2/23/1992"}
+           gender={"male"} />
+
+{
+/*
+<Media pictures = {media} />
+
+<Event events = {events} />
+*/
+}
+
+</div>
+
 </div>
 
 </div>
