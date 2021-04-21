a253cee6501a0830b1b07c34f3e433dcefaed619
Nandish Murugeshi <38002198+nandish291@users.noreply.github.com>
2021-04-21 07:22:48 -0400

    Update home.js
    cleanup

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,6 @@ import React from "react";
 import PrimarySearchAppBar from "./utils/navBar"
 import CarouselComp from "./landing-page/carouselComp";
 import Recommendations from "./landing-page/recommendations";
-import Scroller from "./utils/Scorller";
 
 const Home= ()=> {
 
@@ -11,7 +10,6 @@ const Home= ()=> {
             <PrimarySearchAppBar/>
             <CarouselComp/>
             <Recommendations/>
-            <Scroller/>
         </div>
         )
 }
